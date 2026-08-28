a36d3b209afe9fdb32c53a63f547b009fcacb107
Denis <140589513+Homiakus@users.noreply.github.com>
2026-08-28 15:19:02 +0300

    feat(lbc): expose LBC workflow atlas view

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import LbcWorkflowApp from './LbcWorkflowApp.tsx';
 import { ThemeProvider } from './context/ThemeContext.tsx';
 import './index.css';
+
+const params = new URLSearchParams(window.location.search);
+const showLbcWorkflowAtlas = params.get('view') === 'lbc' || window.location.hash === '#lbc';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      {showLbcWorkflowAtlas ? <LbcWorkflowApp /> : <App />}
     </ThemeProvider>
   </StrictMode>,
 );
